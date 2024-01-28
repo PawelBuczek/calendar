e2d9824993fc83d13e8ab9a010d186e4086f0be2
PawelBuczek <pawel.buczek@email.com>
2024-01-28 14:13:04 +0100

    created IrregularEvent

---
+++ src/main/java/com/pb/calendar/event/IrregularEvent.java
@@ -0,0 +1,6 @@
+package com.pb.calendar.event;
+
+public class IrregularEvent extends Event {
+    private int reccuringEventId;
+    //this event is only used in RecurringEvents as a way to store exceptions to the recurrence pattern
+}
--- src/main/java/com/pb/calendar/event/RecurringEvent.java
+++ src/main/java/com/pb/calendar/event/RecurringEvent.java
@@ -1,8 +1,10 @@
 package com.pb.calendar.event;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class RecurringEvent extends Event {
     private LocalDateTime recurrenceInterval;
     private int recurrenceCount;
+    private List<IrregularEvent> irregularEvents;
 }
