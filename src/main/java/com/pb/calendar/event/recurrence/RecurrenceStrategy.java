21b03f0243e59cd59538e09aff974b0d7c4b851c
PawelBuczek <pawel.buczek@email.com>
2024-01-29 20:10:36 +0100

    added some comments

--- src/main/java/com/pb/calendar/event/recurrence/RecurrenceStrategy.java
+++ src/main/java/com/pb/calendar/event/recurrence/RecurrenceStrategy.java
@@ -2,8 +2,8 @@ package com.pb.calendar.event.recurrence;
 
 import com.pb.calendar.event.OneTimeEvent;
 
-import java.time.LocalDateTime;
-
 public interface RecurrenceStrategy {
     OneTimeEvent nextEvent(OneTimeEvent previousEvent);
+    // would be good to have some way of checking if created recurrence strategy is correct
+    // and doesn't for example produce infinite loops of events
 }
--- src/main/java/com/pb/calendar/service/EventService.java
+++ src/main/java/com/pb/calendar/service/EventService.java
@@ -4,6 +4,7 @@ import com.pb.calendar.event.Event;
 import com.pb.calendar.event.OneTimeEvent;
 import com.pb.calendar.event.recurrence.RecurrenceStrategy;
 import com.pb.calendar.event.recurrence.RecurringEvent;
+import org.springframework.security.core.parameters.P;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -18,12 +19,15 @@ public class EventService {
         return new ArrayList<>();
     }
 
-    List<OneTimeEvent> getOneTimeEvents(RecurringEvent recurringEvent) {
+    List<OneTimeEvent> getOneTimeEvents(RecurringEvent recurringEvent) throws Exception {
         List<OneTimeEvent> events = new ArrayList<>();
         OneTimeEvent latestEvent = recurringEvent.getFirstEvent();
         RecurrenceStrategy strategy = recurringEvent.getRecurrenceStrategy();
         LocalDateTime recurrenceEndTime = recurringEvent.getEndTime();
         while (latestEvent.getEndTime().isBefore(recurrenceEndTime)) {
+            if (events.contains(latestEvent)) {
+                throw new Exception("bad bad situation"); // some better Exception needed
+            }
             events.add(latestEvent);
             latestEvent = strategy.nextEvent(latestEvent);
         }
