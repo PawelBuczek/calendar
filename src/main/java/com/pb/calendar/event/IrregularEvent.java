ec2ffc5015560feb904dc98e03afff82076eb7c6
PawelBuczek <pawel.buczek@email.com>
2024-01-29 11:11:18 +0100

    added IrregularEventType

--- src/main/java/com/pb/calendar/event/IrregularEvent.java
+++ src/main/java/com/pb/calendar/event/IrregularEvent.java
@@ -2,5 +2,6 @@ package com.pb.calendar.event;
 
 public class IrregularEvent extends Event {
     private int reccuringEventId;
+    private IrregularEventType irregularEventType;
     //this event is only used in RecurringEvents as a way to store exceptions to the recurrence pattern
 }
