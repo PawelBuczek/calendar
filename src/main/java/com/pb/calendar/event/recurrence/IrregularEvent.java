9b65356302d17fb8e4777de56987303dd6e23a25
PawelBuczek <pawel.buczek@email.com>
2024-01-30 10:02:46 +0100

    enhanced constructors

--- src/main/java/com/pb/calendar/event/recurrence/IrregularEvent.java
+++ src/main/java/com/pb/calendar/event/recurrence/IrregularEvent.java
@@ -2,15 +2,29 @@ package com.pb.calendar.event.recurrence;
 
 import com.pb.calendar.event.Event;
 import com.pb.calendar.event.EventCategory;
+import com.pb.calendar.event.OneTimeEvent;
 
 import java.time.LocalDateTime;
 
 public class IrregularEvent extends Event {
-    private int reccuringEventId;
+    private int recurringEventCounter;
     private IrregularEventType irregularEventType;
 
-    public IrregularEvent(int createdByUserId, String summary, String details, EventCategory category, LocalDateTime timeCreated, LocalDateTime timeLastEdited, LocalDateTime startTime, LocalDateTime endTime) {
-        super(createdByUserId, summary, details, category, timeCreated, timeLastEdited, startTime, endTime);
+    public IrregularEvent(int recurringEventCounter, int createdByUserId, String summary, String details, EventCategory category, LocalDateTime startTime, LocalDateTime endTime, IrregularEventType irregularEventType) {
+        super(createdByUserId, summary, details, category, startTime, endTime);
+        this.recurringEventCounter = recurringEventCounter;
+        this.irregularEventType = irregularEventType;
+    }
+
+    public IrregularEvent(int recurringEventCounter, IrregularEventType irregularEventType, OneTimeEvent event) {
+        super(event.getCreatedByUserId(),
+                event.getSummary(),
+                event.getDetails(),
+                event.getCategory(),
+                event.getStartTime(),
+                event.getEndTime());
+        this.recurringEventCounter = recurringEventCounter;
+        this.irregularEventType = irregularEventType;
     }
     //this event is only used in RecurringEvents as a way to store exceptions to the recurrence pattern
 }
