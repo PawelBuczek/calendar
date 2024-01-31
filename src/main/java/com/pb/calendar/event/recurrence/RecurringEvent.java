ae09ab62cebb195226f9f5a7a73563e85c99d6e8
PawelBuczek <pawel.buczek@email.com>
2024-01-31 09:34:42 +0100

    added comment for potential improvement

--- src/main/java/com/pb/calendar/event/recurrence/RecurringEvent.java
+++ src/main/java/com/pb/calendar/event/recurrence/RecurringEvent.java
@@ -13,7 +13,11 @@ public class RecurringEvent {
     private OneTimeEvent firstEvent;
     private RecurrenceStrategy recurrenceStrategy;  // pattern of recurrence
     private int recurrenceCount; // if 0 then endless
+
+    // actually it would be better to have just the time ranges where recurring events cannot occur
+    // then additional events would be in the list and there would be no need for recurringEventCounter
     private List<IrregularEvent> irregularEvents; // exceptions to the pattern
+
     private LocalDateTime timeCreated;
     private LocalDateTime timeLastEdited;
     private LocalDateTime startTime;
